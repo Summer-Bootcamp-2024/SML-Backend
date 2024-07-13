6dd6ae621fd0a497a9c14d72dc738d3de4db06db
bkw535 <bkw54570@gmail.com>
2024-07-14 00:48:28 +0900

    Feat: 일촌 및 이촌 리스트 출력 API

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from Backend.backend.api.endpoints import friends, auth, users
+from Backend.backend.api.endpoints import friends, auth, users, relationships
 
 api_router = APIRouter(prefix="/api/v1", tags=["api"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(friends.router, prefix="/friends", tags=["friends"])
+api_router.include_router(relationships.router, prefix="/relationships", tags=["relationships"])
