f07524506659c2554f0ca2979ecdfda7b3e5826a
bkw535 <bkw54570@gmail.com>
2024-07-17 03:29:09 +0900

    [#39] Feat: 채팅 기능 구현

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from Backend.backend.api.endpoints import friends, auth, users, relationships, chatrooms, search
+from Backend.backend.api.endpoints import friends, auth, users, relationships, chatrooms, search, messages
 
 api_router = APIRouter(prefix="/api/v1", tags=["api"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
@@ -9,3 +9,4 @@ api_router.include_router(friends.router, prefix="/friends", tags=["friends"])
 api_router.include_router(relationships.router, prefix="/relationships", tags=["relationships"])
 api_router.include_router(chatrooms.router, prefix="/chatrooms", tags=["chatrooms"])
 api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(messages.router, prefix="/messages", tags=["messages"])
